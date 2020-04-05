ef71630503af8622af921d355b34d5b54b959e87
colinkcurtis <colinkcurtis@gmail.com>
2020-04-05 01:01:07 -0400

    breaking up import/export of files

--- first_draft.py
+++ first_draft.py
@@ -8,8 +8,20 @@ class Importer:
 
     @staticmethod
     def import_excel(input_file_name):
+        """
+        read input files from excel, at least for this demonstration
+        """
         new_file = pandas.read_excel(input_file_name)
         return new_file
+
+    @staticmethod
+    def export_json(export_file_name):
+        """
+        export to json format, probably a decent method for the foreseeable future
+        """
+
+        test = 5
+        return
 
 
 if __name__ == "__main__":
@@ -18,11 +30,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=" henlo2")
 
     parser.add_argument(
-        "-f", "--file", help="input file", default="./data/test_input.xlsx",
+        "-f", "--file", help="input file", default="./data/new_test_input.xlsx",
     )
 
     args = parser.parse_args()
 
     if args.file.endswith(".xlsx"):
-        test1 = ImporterObject.import_excel(args.file)
-        print(type(test1))
+        new_file = ImporterObject.import_excel(args.file)
+        print(new_file.head())
