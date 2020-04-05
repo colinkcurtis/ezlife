ddf9160eda6d13514ab3ea853815172d0f7a73c9
colinkcurtis <colinkcurtis@gmail.com>
2020-04-05 02:38:56 -0400

    blah

--- first_draft.py
+++ first_draft.py
@@ -1,5 +1,6 @@
 import pandas
 import argparse
+import matplotlib.pyplot as plt
 
 
 class Importer:
@@ -11,7 +12,17 @@ class Importer:
         """
         read input files from excel, at least for this demonstration
         """
-        new_file = pandas.read_excel(input_file_name)
+        new_file = pandas.read_excel(input_file_name, index_col=0)
+        # sorted_by_percentage = new_file.sort_values(["%"], ascending=False)
+        return new_file
+
+    @staticmethod
+    def import_csv(input_file_name):
+        """
+        read input files from excel, at least for this demonstration
+        """
+        new_file = pandas.read_csv(input_file_name, index_col=0)
+        # sorted_by_percentage = new_file.sort_values(["%"], ascending=False)
         return new_file
 
     @staticmethod
@@ -20,21 +31,36 @@ class Importer:
         export to json format, probably a decent method for the foreseeable future
         """
 
-        test = 5
         return
 
 
 if __name__ == "__main__":
     ImporterObject = Importer()
 
-    parser = argparse.ArgumentParser(description=" henlo2")
+    parser = argparse.ArgumentParser(description=" hello")
 
     parser.add_argument(
-        "-f", "--file", help="input file", default="./data/new_test_input.xlsx",
+        "-f", "--file", help="input file", default="./data/test_file_csv.csv",
     )
 
     args = parser.parse_args()
 
     if args.file.endswith(".xlsx"):
         new_file = ImporterObject.import_excel(args.file)
+        # new_file.to_csv("./test_file_csv.csv")
+        new_file.head(10).plot(kind="barh")
+        plt.show()
+        print()
+        print(new_file.shape)
+        print()
+        print(new_file.head())
+
+    elif args.file.endswith(".csv"):
+        new_file = ImporterObject.import_csv(args.file)
+        # new_file.to_csv("./test_file_csv.csv")
+        new_file.head(10).plot(kind="barh")
+        plt.show()
+        print()
+        print(new_file.shape)
+        print()
         print(new_file.head())
